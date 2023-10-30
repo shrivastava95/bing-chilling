1df4c0d1ae185ea3a8ce10c4398f7cc5f97a317f
Ishaan Shrivastava <shrivastava.9@iitj.ac.in>
2023-10-30 11:19:19 +0530

    Script without OOP wrapper

--- sample.py
+++ sample.py
@@ -1,186 +1,190 @@
+# import webdriver 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.edge.options import Options
+# from cookies import cookies
 from time import sleep, time
+import pyautogui
 import random
 from io import BytesIO
 import win32clipboard
 from PIL import Image
 
 
-class BingBot:
+def send_to_clipboard(clip_type, data):
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(clip_type, data)
+    win32clipboard.CloseClipboard()
 
-    def __init__(self):
-        self.driver = self.load_driver()
+def load_driver():
+    options = Options()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Edge(options=options)
+    return driver
 
-    @staticmethod
-    def send_to_clipboard(clip_type, data):
-        win32clipboard.OpenClipboard()
-        win32clipboard.EmptyClipboard()
-        win32clipboard.SetClipboardData(clip_type, data)
-        win32clipboard.CloseClipboard()
+def change_tone(driver, tone): # must be one of ['balanced', 'creative', 'precise']
+    script = f"""return document.querySelector('cib-serp')
+.shadowRoot.querySelector('cib-conversation')
+.shadowRoot.querySelector('cib-welcome-container')
+.shadowRoot.querySelector('cib-tone-selector')
+.shadowRoot.querySelector('button[class="tone-{tone}"]')"""
+    element = driver.execute_script(script)
+    element.click()
 
-    def load_driver(self):
-        options = Options()
-        options.add_experimental_option("detach", True)
-        driver = webdriver.Edge(executable_path=r'msedgedriver.exe', options=options)
-        return driver
+def get_text_area(driver):
+    script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-text-input').shadowRoot.querySelector('textarea[name="searchbox"]')"""
+    element = driver.execute_script(script)
+    return element
 
-    def change_tone(self, tone):
-        script = f"""return document.querySelector('cib-serp')
-                    .shadowRoot.querySelector('cib-conversation')
-                    .shadowRoot.querySelector('cib-welcome-container')
-                    .shadowRoot.querySelector('cib-tone-selector')
-                    .shadowRoot.querySelector('button[class="tone-{tone}"]')"""
+def write_text(driver, message):
+    sleep(5)
+    textarea = get_text_area(driver)
+    textarea.click()
+    pyautogui.typewrite(message+'\n', interval=random.randint(1, 2) / 10)
+    pyautogui.press('enter')
 
-        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'cib-serp')))
+def get_button(driver):
+    script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('button[aria-label="Add an image to search"]')"""
+    element = driver.execute_script(script)
+    return element
 
-        element = self.driver.execute_script(script)
-        self.driver.execute_script("arguments[0].scrollIntoView();", element)
-        self.driver.execute_script("arguments[0].click();", element)
+def get_paste_input(driver):
+    # script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-flyout').shadowRoot.querySelector('cib-visual-search').shadowRoot.querySelector('input[class="paste-input"]')"""
+    # script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-flyout').shadowRoot.querySelector('cib-visual-search')"""
+    script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-flyout')"""
+    script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-visual-search').shadowRoot.querySelector('input[class="paste-input"]')"""
+    # script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-flyout').shadowRoot.querySelector('[product="bing"]')"""
+    # script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-flyout')"""
+    # script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('[class="visual-search"]')"""
+    # script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar')"""
+    element = driver.execute_script(script)
+    # children = element.find_elements(By.XPATH, './/*')
+    # for child in children:
+    #     if 'cib-visual-search' in child
+    return element
 
-    def debug_script(self, tone):
-        query_levels = [
-            "document.querySelector('cib-serp')",
-            ".shadowRoot.querySelector('cib-conversation')",
-            ".shadowRoot.querySelector('cib-welcome-container')",
-            ".shadowRoot.querySelector('cib-tone-selector')",
-            f".shadowRoot.querySelector('button[class=\"tone-{tone}\"]')"
-        ]
-        current_query = ""
-        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.TAG_NAME, 'cib-serp')))
+def copy_img_to_clipboard(image_path):
+    image = Image.open(image_path)
+    image.thumbnail((300, 300), Image.ANTIALIAS) # makes the image smaller while maintaining aspect ratio
+    output = BytesIO()
+    image.convert("RGB").save(output, "BMP")
+    data = output.getvalue()[14:]
+    output.close()
+    send_to_clipboard(win32clipboard.CF_DIB, data)
 
-        for level in query_levels:
-            current_query += level
-            element = self.driver.execute_script(f"return {current_query}")
-
-            if element is None:
-                print(f"Failed at query level: {current_query}")
-                return None
-        return element
-
-    def get_text_area(self):
-        script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-text-input').shadowRoot.querySelector('textarea[name="searchbox"]')"""
-        element = self.driver.execute_script(script)
-        return element
-
-    def write_text(self, message):
-        textarea = self.get_text_area()
-        self.driver.execute_script("arguments[0].scrollIntoView();", textarea)
-        self.driver.execute_script("arguments[0].click();", textarea)
-
-        for char in message:
-            sleep(random.randint(1, 3) / 25)
-            textarea.send_keys(char)
-
-        textarea.send_keys(Keys.ENTER)
-
-    def get_button(self):
-        script = """
-            var serp = document.querySelector("#b_sydConvCont > cib-serp");
-            if (!serp) return null;
-            var actionBarMain = serp.shadowRoot.querySelector("#cib-action-bar-main");
-            if (!actionBarMain) return null;
-            return actionBarMain.shadowRoot.querySelector("#camera-container > button");
-        """
-        return self.driver.execute_script(script)
-
-    def get_paste_input(self):
-        script = """return document.querySelector('cib-serp').shadowRoot.querySelector('cib-action-bar').shadowRoot.querySelector('cib-visual-search').shadowRoot.querySelector('input[class="paste-input"]')"""
-        element = self.driver.execute_script(script)
-        return element
-
-    @staticmethod
-    def copy_img_to_clipboard(image_path):
-        image = Image.open(image_path)
-        image.thumbnail((300, 300), Image.LANCZOS)
-        output = BytesIO()
-        image.convert("RGB").save(output, "BMP")
-        data = output.getvalue()[14:]
-        output.close()
-        BingBot.send_to_clipboard(win32clipboard.CF_DIB, data)
-
-    def upload_img(self, message, image_path=None, image_url=None):
-        # Get the camera button
-        button = self.get_button()
-
-        if not button:
-            print("Error: Couldn't fetch the camera button.")
-            return
-
-        # Scroll to and click the camera button
-        self.driver.execute_script("arguments[0].scrollIntoView();", button)
-        self.driver.execute_script("arguments[0].click();", button)
-        sleep(0.2)
-
-        if image_url:
-            # Proceed to paste the image URL
-            paste_input = self.get_paste_input()
-            self.driver.execute_script("arguments[0].scrollIntoView();", paste_input)
-            self.driver.execute_script("arguments[0].click();", paste_input)
-            paste_input.send_keys(image_url)
-            paste_input.send_keys(Keys.ENTER)
-
-        elif image_path:
-            # Existing code to handle the image_path
-            self.copy_img_to_clipboard(image_path)
-            paste_input = self.get_paste_input()
-            self.driver.execute_script("arguments[0].scrollIntoView();", paste_input)
-            self.driver.execute_script("arguments[0].click();", paste_input)
-            paste_input.send_keys(Keys.CONTROL + "v")
-
-        sleep(0.2)
-        self.write_text(message)
-
-    def is_bing_responding(self):
-        script = """
-            var typingIndicator = document.querySelector("#b_sydConvCont > cib-serp")
-                .shadowRoot
-                .querySelector("#cib-action-bar-main")
-                .shadowRoot
-                .querySelector("div > cib-typing-indicator");
-            return typingIndicator.getAttribute('aria-hidden');
-        """
-        return self.driver.execute_script(script) == 'false'
-
-    def get_response(self, limit_counter=1):
-
-        while True:  # Wait until Bing finishes responding
-            if not self.is_bing_responding(): break
-            sleep(1)
-
-        bot_response_script = f"""
-            var turns = document.querySelector('cib-serp')
-            .shadowRoot.querySelector('cib-conversation')
-            .shadowRoot.querySelectorAll('cib-chat-turn')[{limit_counter - 1}]
-            .shadowRoot.querySelector('cib-message-group[class="response-message-group"]')
-            .shadowRoot.querySelectorAll('cib-message[type="text"]');
-
-            var texts = [];
-            for (var i = 0; i < turns.length; i++) {{
-                var shared = turns[i].shadowRoot.querySelector('cib-shared[serp-slot="none"]');
-                if (shared) {{
-                    texts.push(shared.innerText);
-                }}
-            }}
-            return texts;"""
-        try:
-            response = "\n".join(self.driver.execute_script(bot_response_script)).strip()
-            return response
-        except Exception as e:
-            print(e)
-            return "Unable to get response. Try increasing the wait 'delay' or Force Reload Bing"
+def upload_img(driver, message, image_path):
+    copy_img_to_clipboard(image_path)
+    # click on the button
+    button = get_button(driver)
+    button.click()
+    # click on the thing
+    paste_input = get_paste_input(driver)
+    paste_input.click()
+    sleep(5)
+    # copy image to clipboard
+    copy_img_to_clipboard(image_path)
+    # paste image
+    paste_input.send_keys(Keys.CONTROL+"v")
+    # enter message
+    write_text(driver, message)
 
 
-# Usage:
-bot = BingBot()
-bot.driver.get('https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx')
+def get_response(driver, limit_counter=1, wait_time=45): 
+    # refer to https://github.com/deshwalmahesh/Bing-Bot/blob/main/helpers.py to understand limit counter
+    '''
+    Get recent Response from the model. 
+    '''
+    timer_start = time()
+    sleep_time = (wait_time - (time() - timer_start))
+    sleep(max(0, sleep_time))
+    # Have escaped one { with another {
+    bot_response_script = f"""
+var turns = document.querySelector('cib-serp')
+.shadowRoot.querySelector('cib-conversation')
+.shadowRoot.querySelectorAll('cib-chat-turn')[{limit_counter - 1}]
+.shadowRoot.querySelector('cib-message-group[class="response-message-group"]')
+.shadowRoot.querySelectorAll('cib-message[type="text"]');
+
+var texts = [];
+for (var i = 0; i < turns.length; i++) {{{{
+    var shared = turns[i].shadowRoot.querySelector('cib-shared[serp-slot="none"]');
+    if (shared) {{{{
+        texts.push(shared.innerText);
+    }}}}
+}}}}
+return texts;"""
+    try:
+        response = "\n".join(driver.execute_script(bot_response_script)).strip()
+        return response
+    except Exception as e: 
+        print(e)
+        return "Unable to get response. Try increasing the wait 'delay' or Force Reload Bing"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+edgeDriver = load_driver()
+edgeDriver.get('https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx')
+
+# wait until the text area loads.
 sleep(5)
-bot.change_tone('precise') # balanced, creative, precise
-bot.upload_img('what is this', None, 'https://media.discordapp.net/attachments/1049583034170621952/1164958033936400426/IMG_3010.jpg?ex=654e5537&is=653be037&hm=e69fc09b9540a70c3f08710d2f4cdb8cc6659112d242dcdb5ab2216efd7012e6&=&width=895&height=671')
+
+# set the tone to one of "creative", "balanced", "precise"
+tone = 'precise'
+change_tone(edgeDriver, tone)
+
+# upload an image to the thing along with a message
+url = r'streamlit_annotation\streamlit crash course\blob.jpeg'
+message = 'How can AI benefit us?'
+upload_img(edgeDriver, message, url)
+
 # get response
-response = bot.get_response()
+response = get_response(edgeDriver)
 print(response)
+
+# read the message response
+
+# old
+# # type the message into the box
+# write_text(edgeDriver, message)
+
+
+
+# sleep(5)
+
+####################3    
+# def add_all_cookies(driver):
+#     for name, value in cookies.items():
+#         driver.add_cookie({'name': name, 'value': value})
+
+
+
